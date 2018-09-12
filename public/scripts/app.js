25a8bb5ab071eb1c1df3098637a8e8e618d04577
kkwon39 <kkwon39@gmail.com>
2018-09-11 20:28:02 -0400

    Implemeneted createTweet and renderTweet function in app.js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,49 +4,82 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suisdsafadsfasdfadsfasdfasdfadsfadsfadsfasdfadsfadsfaf"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+]
 
 function createTweetElement(tweetData){
   const $tweet = $('<article>').addClass('article-tweet')
+  const time = new Date(tweetData.created_at).toLocaleTimeString();
   const header = `<header class="article-tweet-header">
-                  <img class="profile-pic" src="https://vignette.wikia.nocookie.net/bungostraydogs/images/1/1e/Profile-icon-9.png/revision/latest?cb=20171030104015">
+                  <img class="profile-pic" src="${tweetData.user.avatars.small}">
                   <h2 class="article-header-text">${tweetData.user.name}</h2>
-                </header>`
-  const body = ` <p class="article-content-text">${tweetData.content}</p>`
-  const footer =`   <footer>
-                      <p class="article-tweet-footer">9 days ago</p>
-                      <p class="icons">
-                      <i class="fas fa-flag"></i>
-                      <i class="fas fa-retweet"></i>
-                      <i class="fas fa-heart"></i>
-                      </p>
-                    </footer>`
-
+                  </header>`
+  const body = `<p class="article-content-text">${tweetData.content.text}</p>`
+  const footer =`<footer>
+                    <p class="article-tweet-footer">${time}</p>
+                    <p class="icons">
+                    <i class="fas fa-flag"></i>
+                    <i class="fas fa-retweet"></i>
+                    <i class="fas fa-heart"></i>
+                    </p>
+                 </footer>`;
     $tweet.append(header);
     $tweet.append(body);
     $tweet.append(footer);
-
     return $tweet;
 }
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
+function renderTweets(tweetArray){
+  tweetArray.forEach(function(tweet){
+  $("#tweets-container").append(createTweetElement(tweet));
+  });
 }
 
-var $tweet = createTweetElement(tweetData);
-renderTweets(data);
+$(document).ready(function() {
+    renderTweets(data);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  });
