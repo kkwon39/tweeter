09923bc291c8414da014b28bf0fb8d0aaa97ac1c
kkwon39 <kkwon39@gmail.com>
2018-09-13 20:22:05 +0000

    Added screenshots of working app

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -15,8 +15,8 @@ module.exports = function makeDataHelpers(db) {
         // Get all tweets in `db`, sorted by newest first
         getTweets: function(callback) {
             db.collection("tweets").find().toArray((err, tweets) => {
-                const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-                callback(null, tweets);
+            const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+            callback(null, tweets);
             })
         }
     };
